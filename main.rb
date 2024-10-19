11cb26d5ec8e63ffdd7545530b37ad260ed3c83f
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-10-20 00:26:31 +0800

    Add option to choose roles (maker or breaker)
    Computer breaker not yet implemented

--- main.rb
+++ main.rb
@@ -7,6 +7,22 @@ require_relative 'lib/human'
 # Starts game
 module GameStart
   include Mastermind
-  game = Game.new(ComputerPlayer, HumanPlayer)
+
+  choice = nil
+  until %w[m b].include?(choice)
+    print 'Choose your role: ("m" for code maker and "b" for code breaker) '
+    choice = gets.chomp.downcase
+  end
+
+  case choice
+  when 'm'
+    maker = HumanPlayer
+    breaker = ComputerPlayer
+  else
+    breaker = HumanPlayer
+    maker = ComputerPlayer
+  end
+
+  game = Game.new(maker, breaker)
   game.start
 end
